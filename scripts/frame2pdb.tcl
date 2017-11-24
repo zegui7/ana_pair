4c6256075367483f4bd1d33a8b1f97b88088fdc7
zegui7 <jose.gcp.almeida@gmail.com>
2017-11-24 11:24:00 +0100

    Revert "Revert "for safekeeping""
    This reverts commit 10b2d3a9c30a27a31d463fdd9331c1c9b17e1eed.

--- scripts/frame2pdb.tcl
+++ scripts/frame2pdb.tcl
@@ -2,13 +2,19 @@ set coord [lindex $argv 0]
 set traj [lindex $argv 1]
 set filename [lindex $argv 2]
 set inter [lindex $argv 3]
-mol new $coord 
+mol new $coord
 mol addfile $traj waitfor all molid $coord
 file mkdir [format %s/all_frames $inter]
 set allsel [atomselect top protein]
-for {set i 0} {$i < [molinfo top get numframes]} {incr i} {
-	set n [expr $i + 1]
-	set framename [format %s/all_frames/%s_%s.pdb $inter $filename $n]
-	molinfo top set frame $i
-	$allsel writepdb $framename
+puts $tcl_version
+#for {set i 0} {$i < [molinfo top get numframes]} {incr i} {
+for {set i 0} {$i < 3} {incr i} {
+	catch {
+		set n [expr $i + 1]
+		set framename [format %s/all_frames/%s_%s.pdb $inter $filename $n]
+		molinfo top set frame $i
+		$allsel writepdb $framename
+		puts Frame:$n
 	}
+	}
+exit
